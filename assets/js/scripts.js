2c7aab1ec5b51f37831c9bcea54a854ee559dfc9
Basalio-art <basalioervin@gmail.com>
2025-03-20 09:16:31 +0800

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,59 @@
+lottie.loadAnimation ({
+    container: document.getElementById('v-logo'),
+    loop: true,
+    autoplay: true,
+    path: '/assets/json/data.json',
+    renderer: 'svg'
+});
+
+
+
+
+
+
+
+let next = document.querySelector('.arrow-right');
+let prev = document.querySelector('.arrow-left');
+let slide = document.querySelector('.topics .lists');
+let slidePos = 0;
+
+next.addEventListener('click', function() {
+    slidePos -= 273;
+    if (slidePos < -546) {
+        slidePos = 0;
+    }
+    slide.style.cssText = `transform: translateX(${slidePos}px)`;
+});
+
+prev.addEventListener('click', function() {
+    slidePos += 273;
+    if (slidePos > 0) {
+        slidePos = -546;
+    }
+    slide.style.cssText = `transform: translateX(${slidePos}px)`;
+});
+
+
+
+
+const toggle = document.getElementById('dark-mode');
+const label = document.querySelector('.dark-label');
+let a;
+
+label.addEventListener('click', () => {
+    if (toggle.checked) {
+        document.body.classList.remove('dark');
+        a = 1;
+    } else {
+        document.body.classList.add('dark');
+        a = 2;
+    }
+});
+
+if (toggle.checked) {
+    document.body.classList.add('dark');
+    a = 1;
+} else {
+    document.body.classList.remove('dark');
+    a = 2;
+}
